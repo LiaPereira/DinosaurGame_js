5aaf181638ff00cb0c60e12f29e53fe40035f592
Lia Pereira <ldcpereira@gmail.com>
2023-07-25 22:49:48 +0100

    Deleted transform in css. Created the code for the jump in the script.js file with the hability to jump multiple times in a row - still a little clumsy

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 
+var isJumping = false;
 
-function jump(){
-
-    if(character.classList != "animate"){
-        character.classList.add("animate");
-    }
-    
-    setTimeout(function(){
-        character.classList.remove("animate")
-    }, 500); 
-  
+function jump(){ 
+        isJumping = true;
+        var characterTopInicial = character.getBoundingClientRect().top      
+        var goingUp = setInterval(function(){    
+            if (characterTopInicial - character.getBoundingClientRect().top > 50){
+                clearInterval(goingUp);
+                isJumping = false;
+            }  
+            var add_top = character.getBoundingClientRect().top - 5;
+            character.style.top = add_top.toString() + "px";           
+        }, 20);       
+       
 }
+
+
+var goingDown = setInterval(function(){
+    if(character.getBoundingClientRect().top < 150 && isJumping == false){
+        var add_top = character.getBoundingClientRect().top + 1;
+         character.style.top = add_top.toString() + "px";   
+    }
+
+ },25)
 
 var collisionDetection = setInterval(function(){
     var rect_character = character.getBoundingClientRect();
     var rect_block = block.getBoundingClientRect();
     if (rect_character.right > rect_block.left && rect_character.left < rect_block.right && rect_character.bottom > rect_block.top) {
         block.style.animation = "none";
-        alert("you lost");
+        //alert("you lost");
     }
 }, 10);
